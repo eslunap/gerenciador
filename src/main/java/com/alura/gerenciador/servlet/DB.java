56819cbd937e32b9055f2554fb0bf16ddf9ffc38
eslunap <estefany.luna@gmail.com>
2023-06-01 23:04:40 -0500

    Update DB.java
    add métodos eliminación y buscar

--- src/main/java/com/alura/gerenciador/servlet/DB.java
+++ src/main/java/com/alura/gerenciador/servlet/DB.java
@@ -1,6 +1,7 @@
 package com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DB {
@@ -27,6 +28,26 @@ public class DB {
 	
 	public List<Empresa> getEmpresas(){
 		return DB.listaEmpresas;
+	}
+	
+	public void eliminarEmpresa(Integer id) {
+		
+		Iterator<Empresa> it =listaEmpresas.iterator();
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscarEmpresaPorId(Integer id) {
+		for (Empresa empresa : listaEmpresas) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 
 }
