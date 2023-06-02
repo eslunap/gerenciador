7e64d82de8435730be8074c82b1e2314b119cf21
eslunap <estefany.luna@gmail.com>
2023-06-01 23:10:56 -0500

    Update NuevaEmpresaServlet.java

--- src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
+++ src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -1,18 +1,16 @@
 package com.alura.gerenciador.servlet;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class NuevaEmpresaServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
