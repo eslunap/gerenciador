dd3223eb0e19382e86eb087e1a075f05b20e0320
eslunap <estefany.luna@gmail.com>
2023-06-01 21:13:42 -0500

    update servlet nueva empresa
    se agrega parametro fecha apertura
    manejo try catch y casteo string a Date

--- src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
+++ src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -8,16 +8,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NuevaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("nueva empresa registrada");
+		
 		String nombreEmpresa = request.getParameter("nombre");
+		String paramFechaApertura = request.getParameter("fecha");
+		Date fechaApertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			fechaApertura = sdf.parse(paramFechaApertura);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
 		
 		Empresa empresa = new Empresa();
 		empresa.setNombre(nombreEmpresa);
+		empresa.setFechaApertura(fechaApertura);
 		
 		DB baseDeDatos = new DB(); 
 		baseDeDatos.agregarEmpresa(empresa);
