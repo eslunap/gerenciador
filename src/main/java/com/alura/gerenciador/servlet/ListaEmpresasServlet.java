7aba019291be9e7fe4a0c6d4623b361fea3d9459
eslunap <estefany.luna@gmail.com>
2023-06-01 23:08:56 -0500

    Update ListaEmpresasasServlet.java
    remove imports

--- src/main/java/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -2,17 +2,12 @@ package com.alura.gerenciador.servlet;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
-/**
- * Servlet implementation class ListaEmpresasServlet
- */
 public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -23,6 +18,7 @@ public class ListaEmpresasServlet extends HttpServlet {
 		List<Empresa> listaEmpresas = baseDeDatos.getEmpresas();
 		
 		request.setAttribute("listaEmpresas", listaEmpresas);
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
 		rd.forward(request, response);
 				
